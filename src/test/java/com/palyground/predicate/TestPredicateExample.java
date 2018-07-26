add1a8c55ca2a2a8d26034d1663b483f68b18e96
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-07-26 14:36:53 +0300

    add static import

--- src/test/java/com/palyground/predicate/TestPredicateExample.java
+++ src/test/java/com/palyground/predicate/TestPredicateExample.java
@@ -1,22 +1,23 @@
 package com.palyground.predicate;
 
-import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.palyground.predicate.PredicateExample.*;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by andriusbaltrunas on 7/26/2018.
  */
-public class TestPredicateExample{
+public class TestPredicateExample {
 
     private static List<Person> persons;
 
     @BeforeClass
-    public static void before(){
+    public static void before() {
         persons = new ArrayList<>();
         persons.add(new Person(10, "M", "Andrius", "Andrius", "abdrius@test.com", "Kaunas"));
         persons.add(new Person(30, "M", "Tomas", "Daskutis", "tomasD@test.com", "Kaunas"));
@@ -29,38 +30,38 @@ public class TestPredicateExample{
     }
 
     @Test
-    public void testIsMale(){
-        List<Person> response = PredicateExample.filterPerson(persons, PredicateExample.isMale());
+    public void testIsMale() {
+        List<Person> response = filterPerson(persons, isMale());
         assertTrue(response.size() == 5);
     }
 
     @Test
-    public void testIsAdultWomment(){
-        List<Person> response = PredicateExample.filterPerson(persons, PredicateExample.isAdultWomen());
+    public void testIsAdultWomment() {
+        List<Person> response = filterPerson(persons, isAdultWomen());
         assertTrue(response.size() == 2);
     }
 
     @Test
-    public void testisOlderThen(){
-        List<Person> response = PredicateExample.filterPerson(persons, PredicateExample.isOlderThen(30));
+    public void testisOlderThen() {
+        List<Person> response = filterPerson(persons, isOlderThen(30));
         assertTrue(response.size() == 2);
     }
 
     @Test
-    public void testIsNameAndSurnameWhichFirstLetter(){
-        List<Person> response = PredicateExample.filterPerson(persons, PredicateExample.idNameAndSurnameWhichFirstLetter("A"));
+    public void testIsNameAndSurnameWhichFirstLetter() {
+        List<Person> response = filterPerson(persons, idNameAndSurnameWhichFirstLetter("A"));
         assertTrue(response.size() == 1);
     }
 
     @Test
-    public void testIsFromCity(){
-        List<Person> response = PredicateExample.filterPerson(persons, PredicateExample.isFromCity("Kaunas"));
+    public void testIsFromCity() {
+        List<Person> response = filterPerson(persons, isFromCity("Kaunas"));
         assertTrue(response.size() == 3);
     }
 
     @Test
-    public void testisWomenWithoutEmail(){
-        List<Person> response = PredicateExample.filterPerson(persons, PredicateExample.isWomenWithoutEmail());
+    public void testisWomenWithoutEmail() {
+        List<Person> response = filterPerson(persons, isWomenWithoutEmail());
         assertTrue(response.size() == 1);
     }
 }
